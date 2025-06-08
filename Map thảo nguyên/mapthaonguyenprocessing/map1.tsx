b8a4f33473a5529c55dda010f27112b95ebfd1af
unknown <macehelix@protonmail.com>
2025-06-08 16:26:25 +0700

    need to patch map

---
+++ Map thảo nguyên/mapthaonguyenprocessing/map1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dun2" tilewidth="16" tileheight="16" tilecount="1681" columns="41">
+ <image source="map1tiled.png" width="656" height="656"/>
+</tileset>
