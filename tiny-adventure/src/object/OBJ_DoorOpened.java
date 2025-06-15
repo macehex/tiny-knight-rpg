fcf8e000e49158435c935152aee059e4654ee53a
Mace <macehelix@protonmail.com>
2025-06-15 14:11:19 +0700

    minor fixes

--- tiny-adventure/src/object/OBJ_DoorOpened.java
+++ tiny-adventure/src/object/OBJ_DoorOpened.java
@@ -4,21 +4,31 @@ import entity.Entity;
 import main.GamePanel;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.IOException;
 import java.util.Objects;
 
 public class OBJ_DoorOpened extends Entity {
+    private final GamePanel gp;
     public OBJ_DoorOpened(GamePanel gp){
         super(gp);
+        this.gp = gp;
         name = "DoorOpened";
-        down1 = setup("/object/door/door");
         collision = false;
-        //  HIT BOX
-        solidArea.x = 0;
-        solidArea.y = 0;
-        solidArea.width = gp.tileSize;
-        solidArea.height = gp.tileSize;
+        imageIcon = setupAnimatedGif("/object/dooropen",48,32);
+    }
+    @Override
+    public void draw(Graphics2D g2) {
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
+        if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+
+            g2.drawImage(imageIcon.getImage(), screenX, screenY, gp.tileSize*3, gp.tileSize*2, null);
+        }
     }
 
 }
